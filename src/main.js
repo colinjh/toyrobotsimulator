bb8dc12827df0bc8e84585871d67cc441bcdad12
Colin Harkness <colin@bitcraft.com.au>
2017-10-30 22:24:53 +1100

    added compass function

--- src/main.js
+++ src/main.js
@@ -1,17 +1,58 @@
-var input = ['PLACE 0,0, NORTH', 'MOVE', 'REPORT'];
+var currentCompassPos = 'NORTH';
+
+var max_x = 4;
+var max_y = 4
 
 
 var process = {
 
-    Input: function(value) {
-            console.log(value);
+    input: function(value) {
+        switch(value){
+            case "MOVE":
+                robot.move();
+            break;
+            case "LEFT":
+                compass.left();
+            break;
+            case "RIGHT":
+                compass.right();
+            break;
+        }
     }
 }
 
 var compass = {
 
-    direction: function() {
+    left: function() {
+        if (currentCompassPos === 'NORTH') {
+            currentCompassPos === 'WEST';
+        }else if (currentCompassPos === 'EAST') {
+            currentCompassPos === 'NORTH';
+        }else if (currentCompassPos === 'SOUTH') {
+            currentCompassPos === 'EAST';
+        }else if (currentCompassPos === 'WEST') {
+            currentCompassPos === 'SOUTH';
+        }
+        return currentCompassPos;
+    },
+    right: function() {
+        if (currentCompassPos === 'NORTH') {
+            currentCompassPos === 'EAST';
+        }else if (currentCompassPos === 'EAST') {
+            currentCompassPos === 'SOUTH';
+        }else if (currentCompassPos === 'SOUTH') {
+            currentCompassPos === 'WEST';
+        }else if (currentCompassPos === 'WEST') {
+            currentCompassPos === 'NORTH';
+        }
+        return currentCompassPos;
 
+    }
+}
+
+var robot = {
+
+    move: function() {
 
     }
 }
