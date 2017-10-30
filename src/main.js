0d55e5fa2fec93cd521979a264bfc4956afe30ce
Colin Harkness <colin@bitcraft.com.au>
2017-10-30 21:14:46 +1100

    first commit

---
+++ src/main.js
@@ -0,0 +1,17 @@
+var input = ['PLACE 0,0, NORTH', 'MOVE', 'REPORT'];
+
+
+var process = {
+
+    Input: function(value) {
+            console.log(value);
+    }
+}
+
+var compass = {
+
+    direction: function() {
+
+
+    }
+}
